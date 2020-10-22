434af83f81562fb92b9a4ab8165cd02b9b31d8fb
Dennis <alexm08041998>
2020-10-22 14:19:23 -0600

    ?

---
+++ build-reproductor-Desktop_Qt_5_15_1_MinGW_32_bit-Debug/debug/moc_reproductor.cpp
@@ -0,0 +1,95 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'reproductor.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../reproductor/reproductor.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'reproductor.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Reproductor_t {
+    QByteArrayData data[1];
+    char stringdata0[12];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Reproductor_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Reproductor_t qt_meta_stringdata_Reproductor = {
+    {
+QT_MOC_LITERAL(0, 0, 11) // "Reproductor"
+
+    },
+    "Reproductor"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Reproductor[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+void Reproductor::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject Reproductor::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_Reproductor.data,
+    qt_meta_data_Reproductor,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Reproductor::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Reproductor::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Reproductor.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int Reproductor::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ build-reproductor-Desktop_Qt_5_15_1_MinGW_32_bit-Debug/ui_reproductor.h
@@ -0,0 +1,146 @@
+/********************************************************************************
+** Form generated from reading UI file 'reproductor.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_REPRODUCTOR_H
+#define UI_REPRODUCTOR_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QListWidget>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QProgressBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSlider>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Reproductor
+{
+public:
+    QWidget *centralwidget;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QLabel *label;
+    QSpacerItem *verticalSpacer;
+    QPushButton *pushButton;
+    QListWidget *listWidget;
+    QSlider *horizontalSlider;
+    QLabel *label_2;
+    QPushButton *pushButton_2;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label_3;
+    QProgressBar *progressBar;
+    QCheckBox *checkBox;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *Reproductor)
+    {
+        if (Reproductor->objectName().isEmpty())
+            Reproductor->setObjectName(QString::fromUtf8("Reproductor"));
+        Reproductor->resize(800, 600);
+        centralwidget = new QWidget(Reproductor);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayoutWidget = new QWidget(centralwidget);
+        verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(10, 10, 160, 531));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(verticalLayoutWidget);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        verticalLayout->addWidget(label);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
+        pushButton = new QPushButton(verticalLayoutWidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+
+        verticalLayout->addWidget(pushButton);
+
+        listWidget = new QListWidget(centralwidget);
+        listWidget->setObjectName(QString::fromUtf8("listWidget"));
+        listWidget->setGeometry(QRect(185, 11, 601, 431));
+        horizontalSlider = new QSlider(centralwidget);
+        horizontalSlider->setObjectName(QString::fromUtf8("horizontalSlider"));
+        horizontalSlider->setGeometry(QRect(190, 450, 591, 20));
+        horizontalSlider->setOrientation(Qt::Horizontal);
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(190, 480, 51, 31));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(420, 470, 111, 41));
+        horizontalLayoutWidget = new QWidget(centralwidget);
+        horizontalLayoutWidget->setObjectName(QString::fromUtf8("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(510, 530, 291, 26));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        label_3 = new QLabel(horizontalLayoutWidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+
+        horizontalLayout->addWidget(label_3);
+
+        progressBar = new QProgressBar(horizontalLayoutWidget);
+        progressBar->setObjectName(QString::fromUtf8("progressBar"));
+        progressBar->setValue(24);
+
+        horizontalLayout->addWidget(progressBar);
+
+        checkBox = new QCheckBox(horizontalLayoutWidget);
+        checkBox->setObjectName(QString::fromUtf8("checkBox"));
+
+        horizontalLayout->addWidget(checkBox);
+
+        Reproductor->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(Reproductor);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 21));
+        Reproductor->setMenuBar(menubar);
+        statusbar = new QStatusBar(Reproductor);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        Reproductor->setStatusBar(statusbar);
+
+        retranslateUi(Reproductor);
+
+        QMetaObject::connectSlotsByName(Reproductor);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *Reproductor)
+    {
+        Reproductor->setWindowTitle(QCoreApplication::translate("Reproductor", "Reproductor", nullptr));
+        label->setText(QCoreApplication::translate("Reproductor", "Library", nullptr));
+        pushButton->setText(QCoreApplication::translate("Reproductor", "Load LIbrary", nullptr));
+        label_2->setText(QCoreApplication::translate("Reproductor", "Playing:", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("Reproductor", "Play / Pause", nullptr));
+        label_3->setText(QCoreApplication::translate("Reproductor", "Memory Usage", nullptr));
+        checkBox->setText(QCoreApplication::translate("Reproductor", "Paginate", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Reproductor: public Ui_Reproductor {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_REPRODUCTOR_H
